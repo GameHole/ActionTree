85af2780cb0a2405d1a21892b3011f1c435970d5
GameHole <741419087@qq.com>
2021-04-19 11:20:18 +0800

    添加配置 测试

--- Editor/DirectroyWatcher.cs
+++ Editor/DirectroyWatcher.cs
@@ -16,130 +16,15 @@ namespace ActionTree
     public class DirectroyWatcher
     {
         static Dictionary<string, byte[]> buffer = new Dictionary<string, byte[]>();
-        static string configExcel = "Assets/Editor/Configs";
+        internal static string configExcel = "Assets/Editor/Configs";
         static string configTxtPath = "Assets/Resources/Configs";
-        static string backupPath = "Assets/Editor/ConfigBackUp";
         static DirectroyWatcher()
         {
             if (!Directory.Exists(configExcel))
                 Directory.CreateDirectory(configExcel);
             EditorApplication.update += onUpdate;
-            ObjectFactory.componentWasAdded += onAddCmp;
         }
-        private static void onAddCmp(Component obj)
-        {
-            if(obj is IConfigProxyPdr proxyPdr)
-            {
-                GenrateExcel(proxyPdr.GetElementType());
-            }
-        }
-        static void GenrateExcel(Type type)
-        {
-            string name = null;
-            var fileAtt = type.GetCustomAttribute<FileName>();
-            if (fileAtt != null)
-            {
-                name = fileAtt.name;
-            }
-            else
-            {
-                name = type.FullName;
-            }
-            string path = Path.Combine(configExcel, $"{name}.xlsx");
-            if (!File.Exists(path))
-                File.Create(path).Dispose();
-            var currinfo = new FileInfo(path);
-            using (ExcelPackage package = new ExcelPackage(currinfo))
-            {
-                var fields = type.GetFields();
-                ExcelWorksheet worksheet = package.Workbook.Worksheets["info"];
-                if (worksheet == null|| fields.Length>worksheet.Cells.Columns)
-                {
-                    WriteTitle(package, type, fields);
-                    package.Save();
-                }
-                else
-                {
-                    var strFields = DataDealer.ToStringArray(fields);
-                    if (!isMatch(worksheet, strFields))
-                    {
-                        if (!Directory.Exists(backupPath))
-                            Directory.CreateDirectory(backupPath);
-                        string backPath = Path.Combine(backupPath, $"{name}_{DateTime.Now.ToString("yyyy-mm-dd")}.xlsx");
-                        File.Copy(path, backPath);
-                        File.Delete(path);
-                        using (ExcelPackage newPack = new ExcelPackage(new FileInfo(path)))
-                        {
-                            WriteTitle(newPack, type, fields);
-                            var newsheet = newPack.Workbook.Worksheets["info"];
-                            WriteDatas(worksheet, newsheet, strFields);
-                            newPack.Save();
-                        }
-                    }
-                }
-               
-            }
-            AssetDatabase.Refresh();
-            Debug.Log($"File ({name}) Loaction = {path}");
-        }
-        static void WriteDatas(ExcelWorksheet sor,ExcelWorksheet dest,string[] fieldStrs)
-        {
-            for (int i = 0; i < fieldStrs.Length; i++)
-            {
-                int keyId = indexOf(sor, fieldStrs[i]);
-                if (keyId > 0)
-                {
-                    for (int r = 1; r < 30; r++)
-                    {
-                        if (string.IsNullOrEmpty(sor.Cells[r + 1, keyId].ToString()))
-                            break;
-
-                        Debug.Log($"c = {sor.Cells.Rows} r {r + 1},c{i + 1} {dest.Cells[r + 1, i + 1].Value} , {sor.Cells[r + 1, keyId].Value}");
-                        dest.Cells[r + 1, i + 1].Value = sor.Cells[r + 1, keyId].Value;
-                      
-                    }
-                }
-            }
-        }
-        static int indexOf(ExcelWorksheet sor,string key)
-        {
-            for (int i = 0; i < sor.Cells.Columns - 1; i++)
-            {
-               
-                if (sor.Cells[2, i + 1].Value == null)
-                    break;
-
-                Debug.Log($"c {sor.Cells.Columns}  i {i} {sor},{sor.Cells[2, i + 1].Value} { key}");
-                if (sor.Cells[2, i + 1].Value.ToString() == key)
-                {
-                    return i + 1;
-                }
-            }
-            return -1;
-        }
-        static void WriteTitle(ExcelPackage package, Type type, FieldInfo[] fields)
-        {
-            var worksheet = package.Workbook.Worksheets.Add("info");
-            worksheet.Cells[1, 1].Value = type.FullName;
-            for (int i = 0; i < fields.Length; i++)
-            {
-                string typeStr = DataDealer.ToString(fields[i].FieldType);
-                worksheet.Cells[2, i + 1].Value = $"{typeStr} {fields[i].Name}";
-            }
-        }
-        static bool isMatch(ExcelWorksheet worksheet,string[] fields)
-        {
-            if (fields.Length < worksheet.Cells.Columns)
-                return false;
-            for (int i = 0; i < fields.Length; i++)
-            {
-                if (fields[i] != worksheet.Cells[2, i].Value.ToString())
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
+        
         [MenuItem("Excel/Load All")]
         static void LoadAll()
         {
@@ -157,7 +42,7 @@ namespace ActionTree
         static void onUpdate()
         {
             count++;
-            if (count >= 70)
+            if (count >= 30)
             {
                 //stopwatch.Restart();
                 RepleaseChangedFile();
@@ -224,7 +109,7 @@ namespace ActionTree
                 if(isMapping(item) && isFileChange(item,out var md5))
                 {
                     buffer[item] = md5;
-                    Debug.Log("change");
+                    //Debug.Log("change");
                     RepleaseConfig(item);
                 }
             }
@@ -241,11 +126,14 @@ namespace ActionTree
             {
                 for (int i = 0; i < dataset.Tables.Count; i++)
                 {
-                    DealTable(dataset.Tables[i]);
+                    if (!DealTable(dataset.Tables[i]))
+                    {
+                        Debug.LogWarning($"Config file {path},it doesn't contain anything,so configuration files are not generated ");
+                    }
                 }
             }
         }
-        static void DealTable(DataTable table)
+        static bool DealTable(DataTable table)
         {
             if (table.Rows.Count > 3)
             {
@@ -274,8 +162,10 @@ namespace ActionTree
                     if (!File.Exists(targetPath))
                         File.Create(targetPath).Dispose();
                     File.WriteAllText(targetPath, builder.ToString());
+                    return true;
                 }
             }
+            return false;
         }
         static bool isEmptyLine(object[] a)
         {
