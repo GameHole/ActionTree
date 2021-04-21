4e9d2438171a041347bc701293f9b64494a2b73c
GameHole <741419087@qq.com>
2021-04-21 21:42:21 +0800

    添加动画

---
+++ Assets/Common/Runtime/Functions/Animation/RotateTo/RotateToLeaf.cs
@@ -0,0 +1,16 @@
+﻿using ActionTree;
+using UnityEngine;
+namespace ActionTree
+{
+	public sealed class RotateTo:ATree
+	{
+        RotateToData rotate;
+        Rotation rotation;
+        AnimCurve curve;
+		public override void Do()
+        {
+            rotation.value = Quaternion.SlerpUnclamped(rotate.start, rotate.end, curve.output);
+        }
+	}
+	public class RotateToLeaf: TreeProvider<RotateTo> { }
+}
