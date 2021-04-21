78b0bfd1dda6958e9ba1079142ea230788ef4feb
GameHole <741419087@qq.com>
2021-04-21 21:42:21 +0800

    添加动画

---
+++ Runtime/Functions/Animation/RotateTo/RotateToPdr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using ActionTree;
+using UnityEngine;
+namespace ActionTree
+{
+	[System.Serializable]
+	public sealed class RotateToData : IComponent
+	{
+        public Vector3 startEuler;
+        public Vector3 endEuler;
+        [HideInInspector] public Quaternion start;
+        [HideInInspector] public Quaternion end;
+	}
+	public class RotateToPdr: CmpProvider<RotateToData>
+    {
+        public override IComponent GetValue()
+        {
+            value.start = Quaternion.Euler(value.startEuler);
+            value.end = Quaternion.Euler(value.endEuler);
+            return base.GetValue();
+        }
+    }
+}
